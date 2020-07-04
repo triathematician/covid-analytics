73f12f510f629ea14d21e219b8f4b527964853bf
Elisha Peterson <triathematician@gmail.com>
2020-07-03 21:56:06 -0400

    shift to multi-odule project

--- coda/coda-data/src/test/kotlin/tri/timeseries/RegionTimeSeriesTest.kt
+++ coda/coda-data/src/test/kotlin/tri/timeseries/RegionTimeSeriesTest.kt
@@ -1,14 +1,12 @@
 package tri.timeseries
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.junit.Test
 import tri.regions.RegionLookup
 import tri.util.DefaultMapper
 import java.time.LocalDate
 
 class RegionTimeSeriesTest {
 
-    @Test
     fun testJson() {
         val r = RegionTimeSeries(RegionLookup("Iowa, US"), MetricInfo("test", true, LocalDate.now(), 0, listOf(5, 6)))
         val json = DefaultMapper.prettyPrint(r)
