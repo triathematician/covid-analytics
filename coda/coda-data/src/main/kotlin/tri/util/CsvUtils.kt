922d18fd0676bb922b32bfb4121a4022bc466197
Elisha Peterson <elisha.peterson@jhuapl.edu>
2020-12-29 13:38:13 -0500

    license headers

--- coda/coda-data/src/main/kotlin/tri/util/CsvUtils.kt
+++ coda/coda-data/src/main/kotlin/tri/util/CsvUtils.kt
@@ -33,7 +33,27 @@ object CsvLineSplitter {
 
     /**
      * Reads data from a reader, returning the header line and content lines.
-     * @param splitOnNewLines if true, each line will be read as a separate record; if false (slower), multiple lines will be reconstituted into a single record
+     * @param splitOnNewLines if true, each line will be read as a separate record;
+/*-
+ * #%L
+ * coda-data
+ * --
+ * Copyright (C) 2020 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+ if false (slower), multiple lines will be reconstituted into a single record
      */
     fun readData(splitOnNewLines: Boolean, reader: () -> Reader): Pair<List<String>, Sequence<List<String>>> {
         val header = splitLine(reader().firstLine())!!.map { it.javaTrim() }
