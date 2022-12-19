e9e134628aa015095b6731cae14bcf587360a039
Elisha Peterson <elisha.peterson@jhuapl.edu>
2022-12-19 11:58:56 -0500

    #127,#128 additions to usa library

---
+++ coda/coda-area-usa/src/main/kotlin/tri/area/usa/CbsaFinder.kt
@@ -0,0 +1,44 @@
+/*-
+ * #%L
+ * coda-area-usa-0.5.3-SNAPSHOT
+ * --
+ * Copyright (C) 2020 - 2022 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package tri.area.usa
+
+import tri.util.javaTrim
+
+/** Utility to lookup CBSA's by name. */
+object CbsaFinder {
+    @JvmStatic
+    fun main(vararg args: String) {
+        println("CBSA Finder")
+        println("Purpose: lookup CBSAs by exact or partial names")
+        println("Usage: ./cbsa-finder.sh cbsa1,cbsa2,\"cbsa 3\"")
+        println("-".repeat(20))
+
+        val inputs = args.flatMap { it.split(",") }.map { it.javaTrim() }.toSortedSet()
+        val cbsas = match(inputs)
+        println("CBSA,States,CBSA Code")
+        println(cbsas.map { it.cbsaTitle.replace(", ", ",") + "," + it.cbsaCode }.joinToString("\n"))
+    }
+
+    fun match(inputs: Collection<String>) = Usa.cbsas.values.filter {
+        inputs.any { shortCbsa -> it.cbsaTitle caseInsensitiveContains shortCbsa || it.cbsaCode.toString() == shortCbsa }
+    }
+}
+
+private infix fun String.caseInsensitiveContains(other: String) = toLowerCase().contains(other.toLowerCase())
---
+++ coda/coda-area-usa/src/test/kotlin/tri/area/usa/CbsaFinderTest.kt
@@ -0,0 +1,38 @@
+/*-
+ * #%L
+ * coda-area-usa-0.5.3-SNAPSHOT
+ * --
+ * Copyright (C) 2020 - 2022 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package tri.area.usa
+
+import org.junit.Test
+
+class CbsaFinderTest {
+
+    @Test
+    fun testCbsaFinder() {
+        println(Usa.cbsas.values.filter { it.cbsaTitle.contains(", TX") }.joinToString(", ") { it.cbsaTitle.removeSuffix(", TX") })
+        println(Usa.cbsas.values.filter { it.cbsaTitle.contains(", TX") }.size)
+
+        println(Usa.counties.values.filter { it.parent == Usa.state("TX") }.size)
+        println(Usa.counties.values.filter { it.parent == Usa.state("TX") }.take(123).joinToString(", ") { it.id.removeSuffix(", Texas, US") })
+        println(Usa.counties.values.filter { it.parent == Usa.state("TX") }.take(82).joinToString(", ") { it.id.removeSuffix(", Texas, US") })
+
+        println(UsaAreaLookup.area("District of Columbia, District of Columbia, US"))
+    }
+
+}
